4dc0b33757a36041fa01950185ea24a709d926c1
Ignacio Arrizabalaga <arrizabalaga.ignacio@uni.eus>
2025-12-18 12:18:19 +0100

    Buggy code

--- src/index.php
+++ src/index.php
@@ -1,1 +1,19 @@
-<?php echo 'Hello SonarQube';
+<?php
+
+/**
+ * Simple entry point
+ */
+echo "SonarQube New Code test";
+
+/**
+ * Testik gabeko funtzioa (Coverage jaisteko)
+ * Gainera, bug bat dauka
+ */
+function biderketa($x, $y) {
+    $emaitza = $x * $y;
+
+    // BUG & Code Smell: erabili gabeko aldagaia
+    $erabiliGabe = 123;
+
+    return $x * $y;
+}
